62425b29867ed0fc7199762369a93f8f61d62b0a
Siyue Sun <>
2021-08-13 21:37:06 +0800

    grammer nazi

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">   
       <Header></Header>
       <div class="container">
-        <p class="mb-1">Instruction:</p>
+        <p class="mb-1">Instructions:</p>
         <p>1. Leave the page open for 5min, your data will appear. Sorry I am too poor to buy a backend server, it runs on your browser.</p>
         <p>2. Coins are ranked by 24hrVol vs. 30dAvgVol.</p>
         <p>3. If you do not understand #2, get the hell out of here.</p>
