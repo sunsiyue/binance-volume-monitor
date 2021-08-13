3814eb05ef2235022afdffaf623abf20db76d355
Siyue Sun <>
2021-08-13 21:45:06 +0800

    typo

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
       <Header></Header>
       <div class="container">
         <p class="mb-1">Instructions:</p>
-        <p>1. Leave the page open for 5min, your data will appear. Sorry I am too poor to buy a backend server, it runs on your browser.</p>
+        <p>1. Leave the page open for 5min, your data will appear. I am too poor to buy a backend server, it runs on your browser.</p>
         <p>2. Coins are ranked by 24hrVol vs. 30dAvgVol.</p>
-        <p>3. If you do not understand #2, get the hell outta here.</p>
+        <p>3. If you don't understand #2, get the hell outta here.</p>
       </div>
       <div class="album py-5 bg-light">
         <div class="container">
