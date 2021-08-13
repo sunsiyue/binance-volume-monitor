f5baf3ddeb7bee4aee9bfb31fa854853facf2e09
Siyue Sun <>
2021-08-13 21:37:51 +0800

    fun

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <p class="mb-1">Instructions:</p>
         <p>1. Leave the page open for 5min, your data will appear. Sorry I am too poor to buy a backend server, it runs on your browser.</p>
         <p>2. Coins are ranked by 24hrVol vs. 30dAvgVol.</p>
-        <p>3. If you do not understand #2, get the hell out of here.</p>
+        <p>3. If you do not understand #2, get the hell outta here.</p>
       </div>
       <div class="album py-5 bg-light">
         <div class="container">
